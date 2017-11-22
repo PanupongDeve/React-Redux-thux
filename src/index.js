b65bcd72596444df10fdb0e32311770066908853
PanupongDeve <5635512110@psu.ac.th>
2017-11-23 01:45:22 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore( reducers, {}, applyMiddleware(reduxThunk));
+
+const NotFound = () => <h2>NoteFound</h2>
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route  path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('#root'));
